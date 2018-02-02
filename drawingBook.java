2a4de3a807496822222a77ce5eb635618de317bf
mihir20 <mihir20121997@gmail.com>
2018-02-02 23:30:18 +0530

    drawing book

---
+++ drawingBook.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int solve(int n, int p){
+        if (p<n/2){
+            return p/2;
+        }else{
+            return (n-p)/2;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int p = in.nextInt();
+        int result = solve(n, p);
+        System.out.println(result);
+    }
+}
